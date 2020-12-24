35b60e46833d4f9945d1637b7dc71c0e561c3917
Arif Çakmak <46300706+arifcakmak@users.noreply.github.com>
2020-12-24 19:06:01 +0300

    Initial commit

---
+++ database_check.php
@@ -0,0 +1,21 @@
+<?php
+try {   
+    $db_host = "XXXXX";
+    $db_name = "XXXXX";
+    $db_user = "XXXXX";
+    $db_pass = "XXXXX";
+    $db = new PDO('mysql:host='.$db_host.';dbname='.$db_name, $db_user, $db_pass);
+    echo "Bağlantı başarıyla sağlandı.";
+} 
+catch (PDOException $e){
+    if (strpos($e->getMessage(), "1049") == true){
+        echo "Bağlantıda hatalı veritabanı adı girildi.";
+    }
+    elseif(strpos($e->getMessage(), "1045") == true){
+        echo "Bağlantıda hatalı kullanıcı adı veya şifre girildi.";
+    }
+    elseif(strpos($e->getMessage(), "2002") == true){
+        echo "Bağlantıda hatalı sunucu adresi girildi.";
+    }
+}
+?>
